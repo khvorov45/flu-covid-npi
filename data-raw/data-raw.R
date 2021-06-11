aa79b2d1e8ca91233073ee43dea376ffcd354535
Arseniy Khvorov <khvorov45@gmail.com>
2021-06-11 14:40:53 +1000

    add the right covid table from JHU

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -5,7 +5,16 @@ save_csv <- \(data, name) write_csv(data, glue::glue("data-raw/{name}.csv"))
 read_csv("https://covid19.who.int/WHO-COVID-19-global-data.csv") %>%
   save_csv("covid-cases")
 
-read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19_Unified-Dataset/master/COVID-19_LUT.csv", guess_max = 1e5) %>% save_csv("covid-cases-jhu")
+read_csv(
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19_Unified-Dataset/master/COVID-19_LUT.csv",
+  guess_max = 1e5
+) %>%
+  save_csv("covid-cases-jhu-lut")
+
+download.file(
+  "https://github.com/CSSEGISandData/COVID-19_Unified-Dataset/raw/master/COVID-19.rds","data-raw/covid-cases-jhu.rds",
+  method="curl"
+)
 
 stringency <- httr::GET(
   "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-01-01/2021-12-31",
